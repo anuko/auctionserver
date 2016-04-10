c0996e0fb456642bf3f22a2386ee57ce0c548db9
Nik Okuntseff <support@anuko.com>
2016-04-10 13:25:51 +0000

    Separated hub from server.
    Should probably do different parts as separate projects.

--- src/database/server_mysql.sql
+++ src/database/server_mysql.sql
@@ -1,33 +1,11 @@
 # MySQL-specific code to create tables for auction server.
 
 
-# as_nodes contains nodes this server can connect to in networked mode.
-CREATE TABLE as_nodes (
-  uuid               CHAR(36)       NOT NULL,   # UUID identifying a node.
-  name               VARCHAR(64)    NOT NULL,   # Node name.
-  uri                VARCHAR(256),              # URI at which the node is available.
-  PRIMARY KEY (uuid)
-);
-
-
-# as_node_details contains details about this node.
-CREATE TABLE as_node_details (
+# as_site_details contains details about this installation.
+CREATE TABLE as_site_details (
   uuid               CHAR(36)       NOT NULL,   # UUID identifying this auction site.
-  type               INTEGER,                   # Note type: site, hub, or root hub.
   name               VARCHAR(64)    NOT NULL,   # Name for this site.
-  uri                VARCHAR(256),              # URI at which the server is available to its users.
-  up_nodes           TEXT                       # Comma-separated list of upstream node uuids to talk to.
-);
-
-
-# as_downstream contains downstream nodes that this node talks with.
-CREATE TABLE as_downstream (
-  uuid               CHAR(36)     NOT NULL,     # Node UUID.
-  type               INTEGER,                   # Node type: site or hub.
-  name               VARCHAR(64)  NOT NULL,     # Node name.
-  uri                VARCHAR(256) NOT NULL,     # Node URI.
-  status             INTEGER,                   # Node status.
-  PRIMARY KEY (uuid)
+  uri                VARCHAR(256)               # URI at which the server is available to its users.
 );
 
 
@@ -74,29 +52,4 @@ CREATE TABLE as_bids (
   PRIMARY KEY (uuid)
 );
 # TODO: add indexes.
-
-
-# as_msgs_in contains messages from network received during previous 24 hours.
-# This table is used to filter out redundant messages to process each only once.
-CREATE TABLE as_msgs_in (
-  uuid               CHAR(36)       NOT NULL,   # UUID iidentifying the message.
-  origin             CHAR(36)       NOT NULL,   # UUID of the node the message is from.
-  created_timestamp  CHAR(19)       NOT NULL,   # Creation timestamp in format like "2016-04-08 15:00:10".
-  status             INTEGER,                   # Status of the message
-  PRIMARY KEY (uuid)
-);
-# TODO: add index by timestamp.
-
-
-# as_msgs_out is our outgoing message queue.
-# Successfully sent messages are removed from this table.
-# Messages not yet sent, or not delivered due to failure stay here for a few retries.
-CREATE TABLE as_msgs_out (
-  uuid               CHAR(36)       NOT NULL,   # Random UUID iidentifying the message.
-  remote             CHAR(36)       NOT NULL,   # UUID of destination hub.
-  created_timestamp  CHAR(19)       NOT NULL,   # Creation timestamp in format like "2016-04-08 15:00:10".
-  next_try_timestamp CHAR(19)       NOT NULL,   # timestamp when to try to send it out again.
-  status             INTEGER,                   # Status of the message.
-  PRIMARY KEY (uuid)
-);
 
